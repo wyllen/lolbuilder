30bf32cd48b12140f9f5b87e6f0364f37d189c77
Vincent Durand <vincent.durand@orange.com>
2016-03-17 17:59:01 +0100

    APi test

--- wp-content/themes/lolbuilder/index.php
+++ wp-content/themes/lolbuilder/index.php
@@ -1,6 +1,27 @@
-<header>
-<h1><?php the_title();?></h1>
-</header>
-<div class="wysiwyg-content">
-<?php the_content();?>						
-</div>
+<?php
+
+$aContext = array(
+    'http' => array(
+        'proxy'           => 'nrs-proxy.ad-subs.w2k.francetelecom.fr:3128',
+        'request_fulluri' => true,
+    ),
+);
+$cxContext = stream_context_create($aContext);
+$items     = json_decode(file_get_contents('http://ddragon.leagueoflegends.com/cdn/6.5.1/data/fr_FR/item.json', false, $cxContext));
+
+// var_dump($items->data);
+$i = 0;
+foreach ($items->data as $key => $item) {
+    if (is_null(get_page_by_title($item->name, OBJECT, 'item'))) {
+    	//if($i < 10){
+    		echo $item->name.' a été créé <br>';
+	        $post_id = wp_insert_post(
+	            array(
+	                'post_title'  => $item->name,
+	                'post_status' => 'publish',
+	                'post_type'   => 'item',
+	            ));
+	        $i++;
+    	//}
+    }
+}
